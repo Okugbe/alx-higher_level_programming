eb6d443715eedf296eea579a933c0216001a701f
Okugbe <fsone24@gmail.com>
2023-06-05 17:35:14 -0700

    10-check_cycle.c done

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+/* Definition for singly-linked list */
+typedef struct listint_s {
+    int data;
+    struct listint_s *next;
+} listint_t;
+
+/**
+ * check_cycle - Checks if a singly linked list has a cycle.
+ * @list: Pointer to the head of the linked list.
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *slow, *fast;
+
+    if (list == NULL)
+        return 0;
+
+    slow = list;
+    fast = list->next;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        if (slow == fast)
+            return 1;
+
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return 0;
+}
+
