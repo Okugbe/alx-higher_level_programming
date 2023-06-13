bc328e0677a0695ed3ce162bb5c67457a3889f31
Okugbe <fsone24@gmail.com>
2023-06-13 12:07:03 -0700

    10-divisible_by_2.py done

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+def divisible_by_2(my_list=[]):
+    result = []
+
+    for num in my_list:
+        if num % 2 != 0:
+            result.append(False)
+        else:
+            result.append(True)
+    return (result)
