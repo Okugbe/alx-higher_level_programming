07ebccb5932f5a9a692c07858b30f84821f196c0
Okugbe <egedegbeoghenero@gmail.com>
2023-11-13 20:42:26 +0000

    Testing 7

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const ArgOne = parseInt(process.argv[2]);
+const ArgTwo = parseInt(process.argv[3]);
+
+function add (x, y) {
+  return x + y;
+}
+
+console.log(add(firstArg, secondArg));
+
